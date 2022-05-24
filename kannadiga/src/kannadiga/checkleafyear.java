093e36811aa097e78e2d21ecfda0b8e2ea32d36a
nagabushanv <nagabushanv@DESKTOP-9QBLO94>
2022-05-24 23:20:50 +0530

    commit

---
+++ kannadiga/src/kannadiga/checkleafyear.java
@@ -0,0 +1,50 @@
+package kannadiga;
+import java.util.Scanner;
+
+import java.util.Scanner;
+
+public class checkleafyear {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		System.out.print("Enter any year:");
+		int year  = s.nextInt();
+		boolean flag=false;
+		if(year % 400 == 0)
+		{
+			flag = true;
+		}
+		else if (year % 100 == 0)
+		{
+			flag = false;
+		}
+		else if(year % 4 == 0)
+		{
+			flag = true;
+		}
+		else
+		{
+			flag = false;
+		}
+		if(flag)
+		{
+			System.out.println("Year "+year+" is a leap Year");
+			
+		}
+		else
+		{
+			System.out.println("Year "+year+" ir not a leap Year");
+			
+			
+		}
+
+		
+			
+
+	
+
+
+	}
+
+}
