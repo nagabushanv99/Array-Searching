ca1bbcfd0c17fc2d07cb562b29a51335c875033e
nagabushanv <nagabushanv@DESKTOP-9QBLO94>
2022-05-24 23:54:42 +0530

    commit

---
+++ kannadiga/src/kannadiga/Arraysearching.java
@@ -0,0 +1,37 @@
+package kannadiga;
+import java.util.*;
+
+public class Arraysearching {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int i, n, search, flag=0;
+		System.out.println("Enter the number of elements:");
+		n=sc.nextInt();
+		int[]a=new int[n];
+		System.out.println("Enter the elements:");
+		for(i=1;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		System.out.println("Enter the element to be searched");
+		search=sc.nextInt();
+		for(i=1;i<n;i++)
+		{
+			if(a[i]==search)
+			{
+				System.out.println("Element "+search+" found at "+i+" position");
+				flag=1;
+				break;
+			}
+		}
+		if(flag==0)
+		{
+			System.out.println("Element "+search+" not found");
+		}
+		
+
+	}
+
+}
